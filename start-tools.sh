9f34886b3bc15d47b84145b2910f6a3a768f9751
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2020-11-28 17:22:58 -0500

    Up Java heap size

--- start-tools.sh
+++ start-tools.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-gnome-terminal -e 'java -mx1000m -jar tools/tika-server-1.13.jar --port=9998'  #> /dev/null 2>&1 &
-gnome-terminal -e 'java -mx1000m -cp "tools/stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer' #> /dev/null 2>&1 &
+gnome-terminal -e 'java -mx1000m -jar tools/tika-app-1.24.1.jar --port 9998'  #> /dev/null 2>&1 &
+gnome-terminal -e 'java -mx2000m -cp "tools/stanford-corenlp-4.1.0/*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer' #> /dev/null 2>&1 &
  
